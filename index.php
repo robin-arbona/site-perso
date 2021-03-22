0e8d0b40ce0f76230a55b6ca18701ec591db0a68
Arbona Robin <71873082+robin-arbona@users.noreply.github.com>
2021-03-22 13:26:13 +0100

    first css draft for welcoming animation

--- index.php
+++ index.php
@@ -1,1 +1,29 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="public/style/style.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;700&display=swap" rel="stylesheet">
+    <title>Document</title>
+</head>
+
+<body>
+    <header>
+        <nav></nav>
+        <nav></nav>
+    </header>
+    <main>
+        <section class="section section--m5vh">
+            <div class="title_menu title_menu--w50">
+                <p class="title_menu__content title_menu__content--vertical">Hi,</p>
+            </div>
+        </section>
+    </main>
+    <footer></footer>
+</body>
+
+</html>
